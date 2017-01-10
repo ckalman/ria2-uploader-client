1fb2135a987f33b5cc6975df0b7aa92baae7d972
KALMAN Christophe <Christophe.KALMAN@cpnv.ch>
2017-01-10 13:31:59 +0100

    add bitly constants

--- src/constants/BitlyConstants.js
+++ src/constants/BitlyConstants.js
@@ -4,8 +4,10 @@ export default keyMirror({
   BITLY_INFO: null, // /v3/link/info
   BITLY_SHORTEN: null,
   BITLY_NUMBER_OF_CLICK: null,
+  BITLY_COUNTRIES: null,
   
   BITLY_INFO_ERROR: null,
   BITLY_SHORTEN_ERROR: null,
   BITLY_NUMBER_OF_CLICK: null,
+  BITLY_COUNTRIES_ERROR: null,
 });
