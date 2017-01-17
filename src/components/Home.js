23205e8e604ed0041c9bbfa9c5f101d90213a3a8
KALMAN Christophe <Christophe.KALMAN@cpnv.ch>
2017-01-17 18:17:03 +0100

    first implem drop zone

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import AuthStore from '../stores/AuthStore';
 import BitlyActions from '../actions/BitlyActions';
 import BityStore from '../stores/BitlyStore';
 import Bitly from './url/Bitly';
-import { Form, Button, FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
+import Dropzone from 'react-dropzone';
+import { Form, Button, FormGroup, FormControl, ControlLabel, Panel } from 'react-bootstrap'
 
 class HomeComponent extends React.Component {
 
@@ -31,9 +32,15 @@ class HomeComponent extends React.Component {
         this.setState({ longUrl: e.target.value });
     }
 
-    handleShortCutSubmit() {
+    handleShortCutSubmit(e) {
+        e.preventDefault();
+        console.log(this.state.longUrl);
         BitlyActions.short(this.state.longUrl);
-        //this.setState({ shortcutDisabled: true });
+    }
+
+    onDrop(acceptedFiles, rejectedFiles) {
+        console.log('Accepted files: ', acceptedFiles);
+        console.log('Rejected files: ', rejectedFiles);
     }
 
     onChange() {
@@ -45,16 +52,33 @@ class HomeComponent extends React.Component {
     render() {
         return (
             <div>
-                <h2>Welcome : {this.state.profile.nickname}</h2>
+                <h3 className="center" >Welcome : {this.state.profile.nickname}</h3>
                 <div>
-                    <p> Shortcut your url : </p>
-                    <input type="url" name="longUrl" placeholder="http://google.ch" onChange={this.handleEmailChange} />
-                    <Bitly urls={[this.state.link]}></Bitly>
-                    <p>{this.state.link.url}</p>
-                    <Button onClick={this.handleShortCutSubmit} disabled={this.state.shortcutDisabled}>
-                        shortcut this url
-                            </Button>
-                    <p> or upload photos : </p>
+                    <Panel header="Shortcut your url">
+                        <Form className="center" inline onSubmit={this.handleShortCutSubmit}>
+                            <FormGroup controlId="formInlineEmail">
+                                <FormControl type="url" name="longUrl" placeholder="http://google.ch" onChange={this.handleEmailChange} required />
+                            </FormGroup>
+                            {' '}
+                            <Button type="submit">
+                                shortcut this url
+                        </Button>
+                        </Form>
+
+                        {this.state.link.url != null ? (
+                            <div>
+                                <hr></hr>
+                                <Bitly urls={[this.state.link]}></Bitly>
+                            </div>
+                        ) : (
+                                <div></div>
+                            )}
+                    </Panel>
+                    <Panel header="Upload photos">
+                        <Dropzone onDrop={this.onDrop}>
+                            <div>Try dropping some files here, or click to select files to upload.</div>
+                        </Dropzone>
+                    </Panel>
 
                 </div>
             </div>
